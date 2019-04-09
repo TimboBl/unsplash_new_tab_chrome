f400ca6fcfc62b6538edb2de5da94a8fb4c37c18
Timbo <blaesche.m@gmail.com>
2019-04-09 10:26:05 +0200

    Simplifies the time display

--- extension/scripts/time.js
+++ extension/scripts/time.js
@@ -7,27 +7,15 @@ class Time {
   }
 
   getHours() {
-    let hours = this.time.hours();
-    if (hours < 10) {
-      hours = "0" + hours;
-    }
-    return hours;
+    return this.time.format("HH");
   }
 
   getMinutes() {
-    let minutes = this.time.minutes();
-    if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    return minutes;
+    return this.time.format("mm");
   }
 
   getSeconds() {
-    let seconds = this.time.seconds();
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-    return seconds;
+    return this.time.format("ss");
   }
 
   showTime() {
