fd72667d787968059476df42e835c81c6df4c701
Timbo <blaesche.m@gmail.com>
2019-04-09 09:52:25 +0200

    Removes all python crap

--- backend/chrome_extensions_backend/blueprints/index.py
+++
@@ -1,7 +0,0 @@
-from chrome_extensions_backend import app
-from flask import jsonify
-
-
-@app.route("/")
-def index():
-    return jsonify({"message": "This is in fact a route!"})
--- backend/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="chrome_extensions_backend",
-    packages=["chrome_extensions_backend"],
-    include_package_data=True,
-    install_requires=["flask"],
-)
