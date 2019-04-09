81b7bc86069e911422b41860be39f462ab0071f8
Timbo <blaesche.m@gmail.com>
2019-04-09 11:55:04 +0200

    Adds some config and constant files

---
+++ backend/src/config/constants.ts
@@ -0,0 +1,7 @@
+export const OPEN_WEATHERMAP_API_PATH = "https://api.openweathermap.org/data/2.5";
+
+export const OPEN_WEATHER_MAP_FORECAST_PATH = "/forecast/daily";
+
+export const OPEN_WEATHER_MAP_CURRENT_PATH = "/weather";
+
+export const OPEN_WEATHER_MAP_TOKEN = process.env.OWM_TOKEN;
