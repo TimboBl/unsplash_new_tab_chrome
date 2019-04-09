c9237fd06572eea20108f0c0ef6b276d27745faf
Timbo <blaesche.m@gmail.com>
2019-04-09 10:25:56 +0200

    Adds the new date class

--- extension/scripts/start.js
+++ extension/scripts/start.js
@@ -1,4 +1,8 @@
 window.onload = () => {
   const time = new Time();
+  const date = new DateDisplay();
+  time.showTime();
+  date.showDate();
   const timeInterval = setInterval(() => time.showTime(), 1000);
-}
+  const dateInterval = setInterval(() => date.showDate(), 1000 * 60);
+};
