d4f248b7c26c72d7a1ed3ff708cc4837dbc088a2
Timbo <blaesche.m@gmail.com>
2019-04-11 15:10:28 +0200

    Adds the weather script

--- extension/scripts/start.js
+++ extension/scripts/start.js
@@ -1,8 +1,10 @@
 window.onload = () => {
   const time = new Time();
   const date = new DateDisplay();
+  const weather = new Weather();
   time.showTime();
   date.showDate();
+  setTimeout(() => weather.setWeatherInfo(), 500);
   const timeInterval = setInterval(() => time.showTime(), 1000);
   const dateInterval = setInterval(() => date.showDate(), 1000 * 60);
 };
