3eca2fa1f9061a2f83a3dd262b2587e65460e3b2
Timbo <blaesche.m@gmail.com>
2019-04-18 13:10:41 +0200

    Adds forecast and current weather times

--- backend/src/models/CityId.ts
+++ backend/src/models/CityId.ts
@@ -4,6 +4,8 @@ const CityIdSchema = new Schema({
 	name: {type: String},
 	country: {type: String},
 	id: {type: String},
+	forecastTime: {type: String},
+	currentWeatherTime: {type: String},
 });
 
 CityIdSchema.index({name: 1, country: 1}, {unique: true});
--- backend/src/types/models/CityId.d.ts
+++ backend/src/types/models/CityId.d.ts
@@ -2,4 +2,6 @@ export interface CityIdT {
 	name: string,
 	country: string,
 	id: string,
+	forecastTime: string,
+	currentWeatherTime: string,
 }
