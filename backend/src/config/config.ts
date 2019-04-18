f34c1c59c4829b313ff1c711a313b366212b8622
Timbo <blaesche.m@gmail.com>
2019-04-18 13:10:14 +0200

    Adds a new date format for the current weather

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,6 +2,8 @@ export const PORT = 3000;
 
 export const DATE_FORMAT = "DD.MM.YYYY";
 
+export const CURRENT_WEATHER_DATE_FORMT = "DD.MM.YYYY - HH:mm";
+
 export const OPEN_WEATHER_MAP_TOKEN = process.env.OWM_TOKEN;
 
 export const UNSPLASH_ACCESS_TOKEN = process.env.UNSPLASH_ACCESS_TOKEN;
