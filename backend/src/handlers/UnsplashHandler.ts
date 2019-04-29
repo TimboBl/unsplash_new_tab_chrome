08715f756585daf0d31f53c7a400b6dea3715be9
Timbo <blaesche.m@gmail.com>
2019-04-29 10:43:08 +0200

    Send credits for the images

--- backend/src/handlers/UnsplashHandler.ts
+++ backend/src/handlers/UnsplashHandler.ts
@@ -6,7 +6,13 @@ export const UnsplashHandler = (mongoService: MongoServiceT): UnsplashHandlerT =
 	const getImage = (req: Request, res: Response) => {
 		mongoService.getRandomImage()
 			.then((images => {
-				res.status(200).send({message: "Success", data: images[0].data.links.download});
+				const ret = {
+					image: images[0].data.links.download,
+					user: images[0].data.user.name,
+					twitter: images[0].data.user.twitter_username,
+					instagram: images[0].data.user.instagram_username,
+				};
+				res.status(200).send({message: "Success", data: ret});
 			})).catch((err: Error) =>  {
 				console.error("There was an error when getting a random image from the database", err);
 				res.status(500).send({message: "Internal Server Error"});
